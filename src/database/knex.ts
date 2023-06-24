13736288284d03210500d80f737d0ac67569e512
darmis007 <darshmishra3010@gmail.com>
2023-06-24 17:00:39 +0530

    updated to create db in knex.ts

--- src/database/knex.ts
+++ src/database/knex.ts
@@ -13,4 +13,32 @@ const db = knex({
   },
 });
 
+// Create the database if it doesn't exist
+db.raw(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`).catch((error: Error) => {
+  console.error('An error occurred while creating the database:', error);
+});
+
+// Create the table if it doesn't exist
+db.schema.hasTable('contacts').then((exists: boolean) => {
+  if (!exists) {
+    db.schema
+      .createTable('contacts', (table) => {
+        table.increments('id').primary();
+        table.string('phoneNumber').nullable();
+        table.string('email').nullable();
+        table.integer('linkedId').nullable();
+        table.string('linkPrecedence').notNullable();
+        table.timestamp('createdAt').defaultTo(db.fn.now());
+        table.timestamp('updatedAt').defaultTo(db.fn.now());
+        table.timestamp('deletedAt').nullable();
+      })
+      .then(() => {
+        console.log('Table "contacts" has been created.');
+      })
+      .catch((error: Error) => {
+        console.error('An error occurred while creating the table:', error);
+      });
+  }
+});
+
 export default db;
